4b0662ad1d790a5be3adc65ed2b249ce049a17f0
taiebjb <taiebjebahi17@gmail.com>
2023-04-20 11:59:25 +0000

    stream

--- lib/WelcomeScreen.dart
+++ lib/WelcomeScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-class WelcomeScreen extends StatelessWidget {
+/*class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
   @override
@@ -14,6 +14,64 @@ class WelcomeScreen extends StatelessWidget {
   }
         ,child: Text('sign in'))
       ],),
+    );
+  }
+}*/
+import 'package:flutter/material.dart';
+import 'package:untitled1/widgets/background-image.dart';
+import 'components/rounded_button.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:untitled1/pallete.dart';
+
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+
+      body: Container(
+
+        height: size.height,
+        width: double.infinity,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            BackgroundImage(
+              image: 'assets/images/img.png',
+            ),
+
+
+            SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Welcome to MOTIONEYE !",
+                    style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,color: kBlue,
+
+                    ),
+                  ),
+                  SizedBox(height: size.height * 0.05),
+
+                  SizedBox(height: size.height * 0.05),
+                  RoundedButton(
+                    text: "GET STARTED",
+                    color: kBlue,
+                    textColor: Colors.white,
+                    press: () {
+                        Navigator.pushNamed(context, '/auth');
+                      },)
+
+
+                    ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
